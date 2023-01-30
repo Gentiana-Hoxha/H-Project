a3abc337db2d71e343f2ac13e8eaa47d2cdb845f
Gentiana-Hoxha <60153694+Gentiana-Hoxha@users.noreply.github.com>
2023-01-30 23:04:22 +0100

    move pieces on the board

--- Chess.js
+++ Chess.js
@@ -1,9 +1,8 @@
 // Inserting the Images for the characters
 function insertImage() {
-
     document.querySelectorAll('.box').forEach(image => {
         if (image.innerText.length !== 0) {
-            image.innerHTML = `${image.innerText} <img width="40px" height"40px" class='allimg allpawn' src="${image.innerText}.png" alt="">`
+            image.innerHTML = `${image.innerText} <img width="40px" height="40px" class='allimg allpawn' src="${image.innerText}.png" alt="">`
             image.style.cursor = 'pointer'
         }
     })
@@ -21,6 +20,7 @@ function selectBox() {
                     greenOtherSpaces(box.id, false);
                     selectedBox = box;
                     selectedBoxClassName = box.className;
+
                 }
                 else if(selectedBox != box){
                     greenOtherSpaces(box.id, true);
@@ -30,6 +30,7 @@ function selectBox() {
                     //update new select
                     selectedBox = box;
                 }
+                movePiece(box.id);
             }
         })
     })
@@ -42,14 +43,7 @@ let canMovePathsArrayColumn = [];
 
 function greenOtherSpaces(id, firstTimeClick) {
     if(firstTimeClick){
-        console.log(canMovePathsArrayRow);
-        canMovePathsArrayRow.forEach(canMoveBox => {
-            canMoveBox.className = canMoveBox.className.replace(" green","") 
-        });
-
-        canMovePathsArrayColumn.forEach(canMoveBox => {
-            canMoveBox.className = canMoveBox.className.replace(" green","") 
-        });
+        resetGreenSpace();
     }
 
     selectedBoxFreePaths = id.split(" ");
@@ -60,7 +54,7 @@ function greenOtherSpaces(id, firstTimeClick) {
     rowSelectLeft();
     rowSelectRight();
     columnSelectAbove();
-    columnSelectBelow();
+    columnSelectBelow();    
 
     //row selected
     function rowSelectLeft(){
@@ -89,7 +83,6 @@ function greenOtherSpaces(id, firstTimeClick) {
     //columnSelected
     function columnSelectAbove(){
         let rowAbove = +row + 1;
-        console.log(rowAbove)
         for (let i = rowAbove ; i < 12; i++) {
             let canMoveBox = document.getElementById(`r${i} c${column}`);
             if(canItMoveThere(canMoveBox)){
@@ -106,12 +99,61 @@ function greenOtherSpaces(id, firstTimeClick) {
                 canMovePathsArrayColumn.push(canMoveBox);
                 canMoveBox.className += " green";
             } else break;
-        
         }
     }
-    
 }
 
 function canItMoveThere(element){
     if(element.innerText == '') return true;
 }
+
+let lastClickedElementObject = {
+    type : "",
+    position : ""
+}
+
+let lastClickedElement = null;
+
+function pieceIsClicked(pieceId){
+    lastClickedElement = document.getElementById(pieceId).innerHTML;
+    lastClickedElementObject.type = lastClickedElement.includes("Attack")? "Attack" : "Defend";
+    lastClickedElementObject.position = pieceId;
+    console.log(lastClickedElementObject);
+}
+
+function clickToMove(){
+    document.querySelectorAll('.box.green').forEach( box => {
+        console.log("clicked");
+        box.addEventListener('click', ()=>{
+            box.innerHTML = `${lastClickedElementObject.type} <img width="40px" height="40px" class='allimg allpawn' src="${lastClickedElementObject.type}.png" alt="">`;
+            box.style.cursor = 'pointer';   
+            resetMovedPiece();
+            resetGreenSpace();
+        });
+    })
+}
+function movePiece(id){
+    console.log(id)
+    pieceIsClicked(id)
+    clickToMove();
+}
+
+function resetMovedPiece(){
+    document.getElementById(lastClickedElementObject.position).innerHTML="";
+
+    lastClickedElementObject.type = "";
+    lastClickedElementObject.position = "";
+    lastClickedElement = "";  
+}
+
+
+function resetGreenSpace(){   
+    canMovePathsArrayRow.forEach(canMoveBox => {
+        canMoveBox.className = canMoveBox.className.replace(" green","") 
+    });
+
+    canMovePathsArrayColumn.forEach(canMoveBox => {
+        canMoveBox.className = canMoveBox.className.replace(" green","") 
+    });
+}
+
